38e51bd6dda405bf8c8d2c896bec53e966c6eeea
Luis Godinez <wiichooo@gmail.com>
2018-01-09 18:51:12 -0600

    Curso terminado

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require_relative 'models/product.rb'
+
+get '/' do
+  # HOME LANDING PAGE SHOWING BANNER PHOTO, TITLE, AND SUBTITLE
+  @page_title = "Home"
+  erb :index
+end
+
+
+get '/team' do
+  # TEAM PAGE LISTING THE TEAM MEMBERS
+  @page_title = "Team"
+  erb :team
+end
+
+get '/products' do
+  # PRODUCTS PAGE LISTING ALL THE PRODUCTS
+  @page_title = "Productos"
+  @products = Product.sample_locations
+  erb :products
+end
+
+get '/products/location/:location' do
+  # PAGE DISPLAYING ALL PHOTOS FROM ONE LOCATION
+  @page_title = "Product location"
+  @products = Product.find_by_location(params[:location])
+  erb :product_location
+end
+
+get '/products/:id' do
+  # PAGE DISPLAYING ONE PRODUCT WITH A GIVEN ID
+  @page_title = "Product id"
+  @product = Product.find(params[:id])
+  erb :product_id
+end
---
+++ models/product.rb
@@ -0,0 +1,48 @@
+require 'HTTParty'
+require 'json'
+
+class Product
+  attr_reader :id, :title, :location, :summary, :url, :price
+
+  # ping the API for the product JSON
+  url = 'https://fomotograph-api.udacity.com/products.json'
+  DATA = HTTParty.get(url)['photos']
+
+  LOCATIONS = ['canada', 'england', 'france', 'ireland', 'mexico', 'scotland', 'taiwan', 'us']
+
+
+  # initialize a Product object using a data hash
+  def initialize(product_data = {})
+    @id = product_data['id']
+    @title = product_data['title']
+    @location = product_data['location']
+    @summary = product_data['summary']
+    @url = product_data['url']
+    @price = product_data['price']
+  end
+
+  # return an array of Product objects
+  def self.all
+    DATA.map { |product| new(product) }
+  end
+
+  def self.sample_locations
+    @products = []
+    LOCATIONS.each do |location|
+      @products.push self.all.select { |product| product.location == location }.sample
+    end
+    return @products
+  end
+
+  def self.find_by_location(loc)
+    
+    @products = self.all.select { |product| product.location == loc}
+
+    return @products
+  end
+
+  def self.find(id)
+    return self.all.select { |product| product.id == id.to_i }.first
+  end
+
+end
